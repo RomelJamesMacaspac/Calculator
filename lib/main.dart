54a2d9b68e912aba944b042c0ce13280c5055bbc
CrussyFire_Gaming <rj.macaspac.19@gmail.com>
2024-02-02 03:44:51 +0000

    New Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,105 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController num1 = TextEditingController();
+  TextEditingController num2 = TextEditingController();
+  String ans = '';
+
+  void calculate(String x, String y, String op) {
+    double a = double.parse(x);
+    double b = double.parse(y);
+    double answer;
+    setState(() {
+      switch (op) {
+        case '+':
+          answer = a + b;
+          ans = answer.toString();
+        case '-':
+          answer = a - b;
+          ans = answer.toString();
+        case '*':
+          answer = a * b;
+          ans = answer.toString();
+        case '/':
+          answer = a / b;
+          ans = answer.toString();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+        appBar: AppBar(
+      title: Text('Calculator🧮'),
+      centerTitle: true,
+      backgroundColor: Colors.red,
+    ));
+    body:
+    Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextField(
+            controller: num1,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'First Number:',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: num2,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Second Number:',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    calculate(num1.text, num2.text, '+');
+                  },
+                  child: Text('➕')),
+              TextButton(
+                  onPressed: () {
+                    calculate(num1.text, num2.text, '-');
+                  },
+                  child: Text('➖')),
+              TextButton(
+                  onPressed: () {
+                    calculate(num1.text, num2.text, '*');
+                  },
+                  child: Text('✖')),
+              TextButton(
+                  onPressed: () {
+                    calculate(num1.text, num2.text, '/');
+                  },
+                  child: Text('➗')),
+            ],
+          )
+        ],
       ),
     );
   }
